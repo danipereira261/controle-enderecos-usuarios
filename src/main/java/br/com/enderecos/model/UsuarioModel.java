3a352e74f3313fade27af5c8ac2a1e0c02bcd863
danipereira261 <dpereira261@gmail.com>
2021-05-07 16:18:27 -0300

    22 / 5000 Resultados de tradução database adjustments mvn clean install ;

--- src/main/java/br/com/enderecos/model/UsuarioModel.java
+++ src/main/java/br/com/enderecos/model/UsuarioModel.java
@@ -1,6 +1,7 @@
 package br.com.enderecos.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 
 @Entity
@@ -15,54 +16,8 @@ public class UsuarioModel {
     private String cpf;
     private String dataNascimento;
 
-    public UsuarioModel() {
-    }
+    @OneToMany
+    private List<EnderecoModel> endereco;
 
-    public UsuarioModel(Long id, String nome, String email, String cpf, String dataNascimento) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.cpf = cpf;
-        this.dataNascimento = dataNascimento;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(String dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
 }
